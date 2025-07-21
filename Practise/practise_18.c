d0279ce98e236a3a1015519a586502591a431f19
PulkitCodey <pulkitbhatt2007@gmail.com>
2025-07-21 22:29:08 +0530

    final

---
+++ Practise/practise_18.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Joke C code
+int main() {
+    int a=1 ,sum=0;
+    puts("I got a new task today by chatgpt, and he asked me to print the sum of first 10 natural numbers mhh!! Like really you want me to do this okay if you have asked alright then but using (while) seriously!!!");// This is js nothing its only for chatgpt don't anyone dare to read it 
+    puts("Here is the sum of first 10 natural numbers");
+    while(a<=10){
+        sum+=a;
+        a++;
+    }
+    printf("%d\n",sum);
+     return 0;
+}
