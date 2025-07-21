929fe17ab31d98c5cd5b959e4cb894fe879cfa81
PulkitCodey <pulkitbhatt2007@gmail.com>
2025-07-21 21:42:05 +0530

    final

--- Practise/pracitse_17.c
+++ Practise/pracitse_17.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    puts("Hmm new task for me want to write the ")
+    puts("Hmm new task for me want to write the whole multiplication table backwards fine");
+    int i=0, t=0;// This is done to remove garbage value
+    printf("Just tell me the number of which you want multiplication table of:- ");
+     scanf("%d",&i);
+     printf("Till how many times you want to multiply it?");
+     scanf("%d",&t);
+     printf("The multiplication table of number %d is down below in backwards\n",i);
+     for ( ; t ; t--)
+     {
+//Result
+          printf("%d X %d = %d\n",i,t,i*t);
+     }
      return 0;
 }
