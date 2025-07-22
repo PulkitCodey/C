ddcf4050e88f90997c2b441724aded4754b9bc54
PulkitCodey <pulkitbhatt2007@gmail.com>
2025-07-22 17:19:29 +0530

    working

---
+++ Project_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    do
+    {
+      int Rn, i;
+      puts("HERE'S A GUESS GAME 🎮 FOR YOU. SOUNDS INTERSTING RIGHT. LET'S SEE HOW LONG DOES IT TAKE FOR YOU TO GUESS THE RIGHT NUMBER");
+      
+    } while (choice =='Y'|| choice == 'y'&& choice != 'N' || choice != 'n');
+    
+     return 0;
+}
