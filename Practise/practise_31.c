d4d321c639859ef06929549dbc53677e5d48ea91
PulkitCodey <pulkitbhatt2007@gmail.com>
2025-07-23 14:28:28 +0530

    final

---
+++ Practise/practise_31.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//Simple code to print welcome player in console
+void printBanner(){
+    puts("========================\n"
+            "     Welcome Player\n"
+         "========================\n");
+}
+int main() {
+    printBanner();
+     return 0;
+}
