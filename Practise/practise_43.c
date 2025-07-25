fae9b62624c42cc4ebc7aa0bba6fe0ee71b02619
PulkitCodey <pulkitbhatt2007@gmail.com>
2025-07-25 09:07:14 +0530

    final

--- Practise/practise_43.c
+++ Practise/practise_43.c
@@ -2,6 +2,10 @@
 //A normal code which prints the address of i
 int main() {
     int i;
-    printf("The address of i is %p",(void*)&i);//here 
+    int*j= &i;// The j is a interger pointer which is storing the address of i.
+    printf("The address of i is %p\n",(int*)&i);//here we are just printing the address of i
+    printf("The address of i is %p\n",j);//here we are just printing the address of i. And I didn't add (&) here cause j is pointer which is storing address of j
+    printf("The address of j is %p\n",&j);//Its okay that we know that j is storing the address of i but it doesn't mean that j doesn't have its address 
+    
      return 0;
 }
