04f9039af7883f34a3fb6a9e38f3f6865baca3c4
PulkitCodey <pulkitbhatt2007@gmail.com>
2025-08-01 07:16:23 +0530

    feat: has ability to encrypt given message by increasing ascii value by one

--- Practise/practise_74.c
+++ Practise/practise_74.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
-//String Encrypter by increasing its ascii value
+//String Encrypter by increasing its ascii value.
 void Display();
 void Encrypter(char[]);
 void Decrypter(char[]);
