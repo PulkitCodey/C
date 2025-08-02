dbb41b3334e256d6139c6115bf486806960984bb
PulkitCodey <pulkitbhatt2007@gmail.com>
2025-08-02 13:29:38 +0530

    To show the use of (->) operator in c language.

---
+++ Practise/practise_83.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+// To show the use of (->) operator in c language.
+int main() {
+    typedef struct Employees
+    {
+        char Name[20];
+        char code[10];
+        float salary;
+        char add[100];
+        char dob[20];
+    }Emp;
+    Emp e;
+    Emp *e1= &e;
+    strcpy(e1->Name,"Elliot Alderson");
+    strcpy(e1->code,"ER280652");
+    e1->salary = 900000;
+    strcpy(e1->add,"217 East Broadway, New York, NY 10002, USA");
+    strcpy(e1->dob ,"17/09/1986");
+
+printf("Name %s\n",e1->Name);
+printf("Emp_Code %s\n",e1->code);
+printf("Salary $%.2f\n",e1->salary);
+printf("Address%s\n",e1->add);
+printf("Date of Birth %s\n",e1->dob);
+
+return 0;
+}
