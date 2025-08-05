7f77babfbda66ac305da45c38e7d57e218c49788
PulkitCodey <pulkitbhatt2007@gmail.com>
2025-08-05 22:25:15 +0530

    final

--- Practise/test.c
+++ Practise/test.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-     return 0;
+    while (1) {
+        int *ptr = (int*)malloc(1000000 * sizeof(int));
+
+        // 🟡 ACTUALLY write to memory to force allocation
+        for (int i = 0; i < 1000000; i++) {
+            ptr[i] = i;
+        }
+
+        // No free(ptr); leads to memory leak
+    }
+
+    return 0;
 }
